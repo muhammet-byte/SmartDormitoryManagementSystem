91d498a48f67e14f51c4a62779877f4e4c657f1a
Muhammet Özmen <muhammedozmen06@gmail.com>
2026-04-06 09:52:10 +0300

    Iteration 1(new)

--- src/main/java/com/dormitory/management_system/repository/RoomRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dormitory.management_system.repository;
-
-import com.dormitory.management_system.entity.RoomRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRequestRepository extends JpaRepository<RoomRequest, Long> {
-}
